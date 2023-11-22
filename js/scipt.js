0c107db43836fc3077d4c51bab62420f61314716
balbo97 <balbosauro1@gmail.com>
2023-11-22 15:54:41 +0100

    multipli di 3,5&15 sostituiti

--- js/scipt.js
+++ js/scipt.js
@@ -1,4 +1,20 @@
 //STAMPO I NUMERI DA 1 A 100
 for (let i=1; i<=100; i++){
-    console.log(i)
+    
+    //MULTIPLI DI 3 & 5
+    if(i%15 == 0){
+        console.log('FizzBuzz')
+    }
+    //MULTIPLI DI 3
+    else if(i%3 == 0){
+        console.log('Fizz')
+    }
+    // MULTIPLI DI 5
+    else if(i%5 == 0){
+        console.log('Buzz')
+    }
+    else{
+        console.log(i)
+    }
+    
 }
